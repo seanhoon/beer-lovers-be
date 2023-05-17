cb9078f5af45400efb5eb5f07e7b8d348d637609
seanhoon <sean03pro@gmail.com>
2023-05-17 23:15:53 +0800

    added commandline runner to auto load intial db on startup

--- src/main/java/org/education/beerlovers/user/UserConfig.java
+++ src/main/java/org/education/beerlovers/user/UserConfig.java
@@ -1,41 +1,41 @@
-//package org.education.beerlovers.user;
-//
-//import org.education.beerlovers.beer.Beer;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.Collections;
-//import java.util.List;
-//
-//@Configuration
-//public class UserConfig {
-//
-//
-//  List<Beer> amyBeerList = List.of(
-//    new Beer("corona", 7.0, 8.0),
-//    new Beer("asahi", 8.0, 9.0)
-//  );
-//
-//  List<Beer> benBeerList = List.of(
-//    new Beer("corona", 7.0, 8.0),
-//    new Beer("guiness", 10.0, 9.0)
-//  );
-//
-//  List<Beer> tomBeerList = List.of(
-//    new Beer("heineken", 8.0, 7.0)
-//  );
-//
-//  List<Long> likedByList = Collections.emptyList();
-//
-//  @Bean
-//  CommandLineRunner commandLineRunner(UserRepository userRepository) {
-//    return args -> {
-//      User tom = new User("user1@epam.com", "password", UserRole.USER, likedByList, tomBeerList);
-//      User amy = new User("user2@epam.com", "password", UserRole.USER, likedByList, amyBeerList);
-//      User ben = new User("user3@epam.com", "password", UserRole.USER, likedByList, benBeerList);
-//
-//      userRepository.saveAll(List.of(tom, amy, ben));
-//    };
-//  }
-//}
+package org.education.beerlovers.user;
+
+import org.education.beerlovers.beer.Beer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class UserConfig {
+
+  List<Long> emptyLikedByList = Collections.emptyList();
+
+  private Beer makeBeer(String beerName, Double price, Double score) {
+    return new Beer(beerName, price, score);
+  };
+
+  @Bean
+  CommandLineRunner commandLineRunner(UserRepository userRepository) {
+    Set<Beer> tomBeerList = new HashSet<Beer>();
+    Set<Beer> amyBeerList = new HashSet<Beer>();
+    Set<Beer> benBeerList = new HashSet<Beer>();
+    tomBeerList.add(makeBeer("heineken", 6.0, 6.5));
+    tomBeerList.add(makeBeer("asahi", 7.5, 8.5));
+    amyBeerList.add(makeBeer("corona", 7.0, 8.0));
+    amyBeerList.add(makeBeer("guiness", 10.0, 9.0));
+    benBeerList.add(makeBeer("asahi", 7.0, 8.5));
+
+    return args -> {
+      User tom = new User("user1@epam.com", "password", UserRole.ADMIN, emptyLikedByList, tomBeerList);
+      User amy = new User("user2@epam.com", "password", UserRole.USER, emptyLikedByList, amyBeerList);
+      User ben = new User("user3@epam.com", "password", UserRole.USER, emptyLikedByList, benBeerList);
+
+      userRepository.saveAll(List.of(tom, amy, ben));
+    };
+  }
+}
